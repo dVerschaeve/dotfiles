37d7f7486b52d446d246ce39976798bd9c98aa43
dVerschaeve <dries.verschaeve@vddesign.be>
2022-06-14 08:55:55 +0000

    OhMyPosh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ohmyposh() {
+    sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+    sudo chmod +x /usr/local/bin/oh-my-posh
+
+    mkdir ${HOME}/.poshthemes
+    wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ${HOME}/.poshthemes/themes.zip
+    unzip ${HOME}/.poshthemes/themes.zip -d ${HOME}/.poshthemes
+    chmod u+rw ${HOME}/.poshthemes/*.omp.*
+    rm ${HOME}/.poshthemes/themes.zip
+}
+
+zshrc () {
+    cat linux/.zshrc > $HOME/.zshrc
+}
+
+#ohmyposh
+zshrc
---
+++ linux/.zshrc
@@ -0,0 +1,8 @@
+export ZSH=$HOME/.oh-my-zsh
+ZSH_THEME="codespaces"
+plugins=(git)
+source $ZSH/oh-my-zsh.sh
+DISABLE_AUTO_UPDATE=true
+DISABLE_UPDATE_PROMPT=true
+
+eval "$(oh-my-posh init zsh)"
