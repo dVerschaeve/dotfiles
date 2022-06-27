2fc3143b3de31983a93b40543b88cad30543ae61
Dries VERSCHAEVE <dries.verschaeve@external.engie.com>
2022-06-27 11:07:06 +0200

    PwshProfile

---
+++ common/pwsh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,22 @@
+$env:AZ_ENABLED = $true
+$env:DOTFILES = Join-path $Home ".dotfiles"
+$ENV:OHMYPWSH = "{0}.{1}" -f $PSVersionTable.PSVersion.Major, $PSVersionTable.PSVersion.Minor
+
+
+Import-Module 'Terminal-Icons' -ErrorAction SilentlyContinue
+
+If(Test-Path $Env:DOTFILES){
+	$TerminalPrompt = Join-path $Env:DOTFILES "\common\ohmyposh\terminalprompt.omp.json"
+	oh-my-posh --init --shell pwsh --config $TerminalPrompt | Invoke-Expression
+}
+
+# DOT source workstation related files
+$CustomFolder = Join-Path (Split-Path $profile) "Custom"
+If(Test-Path $CustomFolder){
+	$POSHFiles = Get-ChildItem -Path $CustomFolder -Filter *.ps1
+	ForEach($POSHFile in $POSHFiles){
+		. $POSHFile.FullName
+	}
+}
+
+Clear-Host
--- install-windows.ps1
+++ install-windows.ps1
@@ -21,3 +21,19 @@ Deploy-WingetPackage -PackageName 'JanDeDobbeleer.OhMyPosh'
 Deploy-WingetPackage -PackageName 'Git.Git'
 Deploy-WingetPackage -PackageName 'PuTTY.PuTTY'
 Deploy-WingetPackage -PackageName 'Notepad++.Notepad++'
+
+# Deploy PowerShell Profile files
+$MyDocuments = [Environment]::GetFolderPath("MyDocuments")
+
+$PowerShellFolders = @("PowerShell", "WindowsPowerShell")
+ForEach($PowerShellFolder in $PowerShellFolders){
+    $TargetFolder = Join-Path ($MyDocuments) $PowerShellFolder
+    Write-Host "Copy PowerShell profile to" $TargetFolder
+    If(-Not (Test-Path $TargetFolder)){
+        New-Item -Path $TargetFolder -ItemType Directory -Force | Out-Null
+    } 
+
+    $VSCodeFile = Join-Path $TargetFolder "Microsoft.VSCode_profile.ps1"
+    Copy-Item (Join-Path $CommonFolder 'pwsh\Microsoft.PowerSHell_Profile.ps1') -Destination $TargetFolder -Force -Confirm:$false
+    Copy-Item (Join-Path $CommonFolder 'pwsh\Microsoft.PowerSHell_Profile.ps1') -Destination $VSCodeFile -Force -Confirm:$false
+}
